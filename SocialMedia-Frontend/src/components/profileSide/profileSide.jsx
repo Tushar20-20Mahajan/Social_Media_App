aa99263ce9a08a80bc141cf63561e00bb65d4d0a
Tushar Mahajan <tusharmahajan080@gmail.com>
2024-03-17 12:52:05 +0530

    Changes done making components folder and inside it making profileSide folder and inside it making of profileSide.jsx and profileSide.css added

---
+++ SocialMedia-Frontend/src/components/profileSide/profileSide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const profileSide = () => {
+  return (
+    <div>profileSide</div>
+  )
+}
+
+export default profileSide
---
+++ SocialMedia-Frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='profileSide'>Profile</div>
+        <div className='post'>Post</div>
+        <div className='rightSide'>RightSide</div>
+
+    </div>
+  )
+}
+
+export default Home
